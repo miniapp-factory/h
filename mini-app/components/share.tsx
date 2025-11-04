d8210c368b8675e8468ee8b2b2b810763a1cb95d
miniapp-factory <miniapp-factory@openxai.org>
2025-11-04 15:29:42 +0100

    Initial commit

---
+++ mini-app/components/share.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useMiniAppContext } from "./context/miniapp-provider";
+import { Button } from "./ui/button";
+
+function Share({ text, className }: { text: string; className?: string }) {
+  const { sdk, isInMiniApp } = useMiniAppContext();
+
+  if (!isInMiniApp) {
+    return <></>;
+  }
+
+  return (
+    <Button
+      className={className}
+      onClick={() => {
+        sdk.actions.composeCast({
+          text,
+        });
+      }}
+    >
+      Share
+    </Button>
+  );
+}
+
+export { Share };
